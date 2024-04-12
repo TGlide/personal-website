088665400d5023542a6dedbceca431d74390f238
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2024-04-12 12:58:14 +0100

    fps option

--- src/withers/withRaf.ts
+++ src/withers/withRaf.ts
@@ -1,9 +1,17 @@
-export function withRaf(callback: (FrameRequestCallback)) {
+export function withRaf(callback: FrameRequestCallback, fps: number = 60) {
 	let rafId: number
+	let then = performance.now()
+	const interval = 1000 / fps
+	let delta: number
 
-	function loop(t: number) {
-		callback(t)
+	function loop(now: number) {
 		rafId = requestAnimationFrame(loop)
+		delta = now - then
+
+		if (delta >= interval) {
+			then = now - (delta % interval)
+			callback(delta)
+		}
 	}
 
 	rafId = requestAnimationFrame(loop)

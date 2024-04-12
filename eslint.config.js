941c28457bec532ea515433ca67769e5a75d347f
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2024-04-12 12:58:13 +0100

    bad hero mvp

---
+++ eslint.config.js
@@ -0,0 +1,8 @@
+// eslint.config.js
+import config from '@antfu/eslint-config'
+
+export default config({
+	stylistic: {
+		indent: 'tab',
+	},
+})
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,26 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: '1rem',
+			},
+		},
+		extend: {
+			fontFamily: {
+				sans: ['Inter', 'sans-serif'],
+			},
+			keyframes: {
+				wave: {
+					'0%, 100%': { 'transform': 'rotate(10deg)', 'transform-origin': 'bottom' },
+					'50%': { 'transform': 'rotate(-10deg)', 'transform-origin': 'bottom' },
+				},
+			},
+			animation: {
+				wave: 'wave 1000ms ease-in-out infinite',
+			},
+		},
 	},
 	plugins: [],
 }

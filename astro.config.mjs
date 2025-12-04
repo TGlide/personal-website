ffa438515fe275c71f316cc5e8508033ef5eae8c
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2025-12-04 18:45:45 +0000

    fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,25 @@
-import mdx from '@astrojs/mdx'
+import mdx from "@astrojs/mdx";
 
-import svelte from '@astrojs/svelte'
-import tailwind from '@astrojs/tailwind'
-import { defineConfig } from 'astro/config'
-import Icons from 'unplugin-icons/vite'
-import db from '@astrojs/db'
+import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import Icons from "unplugin-icons/vite";
+import db from "@astrojs/db";
 
-import cloudflare from '@astrojs/cloudflare'
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), svelte(), mdx(), db()],
-	vite: {
-		plugins: [Icons({
-			compiler: 'astro',
-		})],
-	},
-	output: 'hybrid',
-	adapter: cloudflare({
-		imageService: 'compile',
-	}),
-})
+  integrations: [tailwind(), svelte(), mdx(), db()],
+  vite: {
+    plugins: [
+      Icons({
+        compiler: "astro",
+      }),
+    ],
+  },
+  output: "server",
+  adapter: cloudflare({
+    imageService: "compile",
+  }),
+});

36e6af8b4be40f26493db1e0c99af57fe2c1f967
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2025-12-04 19:43:21 +0000

    remove astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import mdx from "@astrojs/mdx";
 
-import db from "@astrojs/db";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
@@ -8,7 +7,7 @@ import Icons from "unplugin-icons/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), mdx(), db()],
+  integrations: [tailwind(), svelte(), mdx()],
   vite: {
     plugins: [
       Icons({
--- db/seed.ts
+++
@@ -1,7 +0,0 @@
-import { PostMeta, db } from 'astro:db'
-
-export default async function () {
-	// await db.insert(PostMeta).values([
-	// 	{ slug: 'debunking-nue-css', views: 10452 },
-	// ])
-}
